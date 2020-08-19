f2877754df91b68db3b877f518aea4363a9b7c64
Nicole O’Bomsawin <nicoleobomsawin@gmail.com>
2020-08-19 10:35:09 -0400

    adding search btn and limiting characters

--- src/components/searchbar/index.js
+++ src/components/searchbar/index.js
@@ -4,7 +4,7 @@ import './index.css';
 function Searchbar() {
     return(
         <div className="searchbar">
-            <input type="text" className="input" placeholder="Enter a zipcode..." />
+            <input type="text" className="input" placeholder="Enter a zipcode..." maxLength="5"/><a href="#"><i class="fas fa-search fa-2x"></i></a>
         </div>
     )
 }
