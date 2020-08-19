373e186333369e0dc3da4a84c70c666474049b00
Nicole O’Bomsawin <nicoleobomsawin@gmail.com>
2020-08-19 10:45:48 -0400

    adding card to news component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './index.css';
 import Searchbar from './components/searchbar/index';
+import News from './components/news/index';
 
 function App() {
   return (
     <div>
       <Searchbar />
+      <News />
     </div>
   );
 }
--- src/components/news/index.js
+++ src/components/news/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import { Card, Button } from 'react-bootstrap';
 
 class News extends React.Component {
     render() {
-        return()
+        return(
+            <div className="card">
+                <Card style={{ width: '18rem', color: 'white' }}>
+                    <Card.Img variant="top" src="holder.js/100px180" />
+                    <Card.Body>
+                        <Card.Title>
+                            
+                        </Card.Title>
+                        <Card.Text>
+                        
+                        </Card.Text>
+                        <Button variant="primary">Read More</Button>
+                    </Card.Body>
+                </Card>
+            </div>
+        );
     }
 }
 
